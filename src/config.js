ab32d563c406a0b3b3a8ef796fcae0919e5e0128
X1 Carbon <yjl9903@vip.qq.com>
2019-12-18 19:35:14 +0800

    update: config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const baseURL = process.env.VUE_APP_URL || 'http://47.99.39.162:3000/';
+export const baseURL = process.env.VUE_APP_URL || 'http://localhost:3000/';
 
-export const wsURL = process.env.VUE_APP_WS_URL || 'ws://47.99.39.162:3000/chat/';
+export const wsURL = process.env.VUE_APP_WS_URL || 'ws://localhost:3000/chat/';
